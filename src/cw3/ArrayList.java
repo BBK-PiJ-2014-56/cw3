dd9bb1a87e375bb21cd9ce772a79c9a4f6aaf37c
BBK-PiJ-2014-56 <jimjohn_thornton@James-Thorntons-MacBook-Pro.local>
2015-01-24 11:04:35 +0000

    Initial commit

---
+++ src/cw3/ArrayList.java
@@ -0,0 +1,165 @@
+package cw3;
+
+/**
+ * Created by jimjohn_thornton on 24/01/15.
+ */
+
+public class ArrayList implements List{
+    private Object[] AL;
+    public ArrayList() {
+        AL = new Object[0];
+    }
+	/*
+	public ArrayList(int index) {
+		AL = new Object[index];
+	}*/
+    /**
+     * Returns true if the list is empty, false otherwise.
+     *
+     * @return true if the list is empty, false otherwise.
+     */
+    public boolean isEmpty() {
+        if (size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Returns the number of items currently in the list.
+     *
+     * @return the number of items currently in the list
+     */
+    public int size() {
+        return AL.length;
+    }
+
+    /**
+     * Returns the elements at the given position.
+     *
+     * If the index is negative or greater or equal than the size of
+     * the list, then an appropriate error must be returned.
+     *
+     * @param index the position in the list of the item to be retrieved
+     * @return the element or an appropriate error message,
+     *         encapsulated in a ReturnObject
+     */
+    public ReturnObject get(int index) {
+        ReturnObjectImpl rtn = new ReturnObjectImpl();
+        if (isEmpty() == true) {
+            rtn.setError(ErrorMessage.EMPTY_STRUCTURE);
+        } else if(index < 0 || index >= size()) {
+            rtn.setError(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        } else {
+            rtn.setObj(AL[index]);
+        }
+        return rtn;
+    }
+
+    /**
+     * Returns the elements at the given position and removes it
+     * from the list. The indeces of elements after the removed
+     * element must be updated accordignly.
+     *
+     * If the index is negative or greater or equal than the size of
+     * the list, then an appropriate error must be returned.
+     *
+     * @param index the position in the list of the item to be retrieved
+     * @return the element or an appropriate error message,
+     *         encapsulated in a ReturnObject
+     */
+    public ReturnObject remove(int index) {
+        ReturnObjectImpl rtn = (ReturnObjectImpl) get(index);
+        if (size() == 0) {
+            rtn.setError(ErrorMessage.EMPTY_STRUCTURE);
+        } else if (index < 0 || index >= size()) {
+            rtn.setError(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        } else {
+
+            Object[] temp = new Object[size() - 1];
+            for (int i=0; i < index; i++) {
+                temp[i] = AL[i];
+            }
+            for (int j = index+1; j < size(); j++) {
+                temp[j-1] = AL[j];
+            }
+            AL = temp;
+        }
+        return rtn;
+
+    }
+
+    /**
+     * Adds an element to the list, inserting it at the given
+     * position. The indeces of elements at and after that position
+     * must be updated accordignly.
+     *
+     * If the index is negative or greater or equal than the size of
+     * the list, then an appropriate error must be returned.
+     *
+     * If a null object is provided to insert in the list, the
+     * request must be ignored and an appropriate error must be
+     * returned.
+     *
+     * @param index the position at which the item should be inserted in
+     *              the list
+     * @param item the value to insert into the list
+     * @return an ReturnObject, empty if the operation is successful
+     *         the item added or containing an appropriate error message
+     */
+    public ReturnObject add(int index, Object item) {
+        ReturnObjectImpl rtn = new ReturnObjectImpl();
+        if (item == null) {
+            rtn.setError(ErrorMessage.INVALID_ARGUMENT);
+        } else if(index < 0 || index > size()) {
+            rtn.setError(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        } else {
+            rtn.setObj(item);
+            extendArray();
+            for (int j = size()-1; j > index; j--) {
+                AL[j] = AL[j-1];
+            }
+            AL[index] = item;
+        }
+        return rtn;
+    }
+
+    /**
+     * Adds an element at the end of the list.
+     *
+     * If a null object is provided to insert in the list, the
+     * request must be ignored and an appropriate error must be
+     * returned.
+     *
+     * @param item the value to insert into the list
+     * @return an ReturnObject, empty if the operation is successful
+     *         the item added or containing an appropriate error message
+     */
+    public ReturnObject add(Object item) {
+        ReturnObjectImpl rtn = new ReturnObjectImpl();
+        if (item == null) {
+            rtn.setError(ErrorMessage.INVALID_ARGUMENT);
+        } else {
+            rtn.setObj(item);
+            extendArray();
+            AL[size()-1] = item;
+        }
+        return rtn;
+    }
+    public void extendArray() {
+        int index = size();
+        Object[] temp = new Object[index + 1];
+        for (int i=0; i < index; i++) {
+            temp[i] = AL[i];
+        }
+        AL = temp;
+    }
+    public void viewArray() {
+        for(int i =0; i< AL.length;i++){
+            System.out.println(i + ":" + AL[i]);
+        }
+        System.out.println("");
+    }
+}
+
---
+++ src/cw3/ArrayListTest.java
@@ -0,0 +1,132 @@
+package cw3;
+
+/**
+ * Created by jimjohn_thornton on 24/01/15.
+ */
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class ArrayListTest {
+
+    ArrayList A1;
+    @Before
+    public void setUp(){
+        A1 = new ArrayList();
+    }
+    @Test
+    public void isEmptyTest() {
+        assertEquals(true, A1.isEmpty());
+        A1.add(3);
+        assertEquals(false, A1.isEmpty());
+        A1.remove(0);
+        assertEquals(true, A1.isEmpty());
+    }
+    @Test
+    public void EqualsTest() {
+        assertEquals(true, A1.isEmpty());
+    }
+
+    @Test
+    public void sizeTest() {
+        assertEquals(0, A1.size());
+        A1.add(3);
+        assertEquals(1, A1.size());
+        A1.add(3);
+        assertEquals(2, A1.size());
+        A1.remove(0);
+        assertEquals(1, A1.size());
+    }
+    @Test
+    public void getTest() {
+        assertEquals(ErrorMessage.EMPTY_STRUCTURE, A1.get(0).getError());
+        A1.add("poopie");
+        A1.add("poos");
+        assertEquals("poopie", A1.get(0).getReturnValue());
+        assertEquals(ErrorMessage.INDEX_OUT_OF_BOUNDS, A1.get(2).getError());
+        assertEquals(ErrorMessage.INDEX_OUT_OF_BOUNDS, A1.get(-1).getError());
+    }
+    @Test
+    public void AddTest() {
+        //testing basic add at end of array
+        A1.add(3);
+        A1.add("stinky");
+        A1.add("poopie");
+        A1.add("poos");
+        assertEquals(3, A1.get(0).getReturnValue());
+        assertEquals("stinky", A1.get(1).getReturnValue());
+        assertEquals("poopie", A1.get(2).getReturnValue());
+        assertEquals("poos", A1.get(3).getReturnValue());
+        assertEquals(ErrorMessage.INDEX_OUT_OF_BOUNDS, A1.get(4).getError());
+        assertEquals(ErrorMessage.INDEX_OUT_OF_BOUNDS, A1.get(-1).getError());
+        assertEquals(ErrorMessage.INVALID_ARGUMENT, A1.add(null).getError());
+        //testing adding a number into an index of the array
+        A1.add(1,4);
+        assertEquals(3, A1.get(0).getReturnValue());
+        assertEquals(4, A1.get(1).getReturnValue());
+        assertEquals("stinky", A1.get(2).getReturnValue());
+        assertEquals("poopie", A1.get(3).getReturnValue());
+        assertEquals("poos", A1.get(4).getReturnValue());
+		/*tests out of bounds conditions note that you can insert an object into position 5 of an
+		array of 4 elements as that will then be the same as adding an object to the end of the
+		array*/
+        assertEquals(ErrorMessage.INDEX_OUT_OF_BOUNDS, A1.add(6,"index too big").getError());
+        assertEquals(ErrorMessage.INDEX_OUT_OF_BOUNDS, A1.add(-1,"index too small").getError());
+        //testing adding a number into the beginning of the array
+        A1.add(0,"start");
+        assertEquals("start", A1.get(0).getReturnValue());
+        assertEquals(3, A1.get(1).getReturnValue());
+        assertEquals(4, A1.get(2).getReturnValue());
+        assertEquals("stinky", A1.get(3).getReturnValue());
+        assertEquals("poopie", A1.get(4).getReturnValue());
+        assertEquals("poos", A1.get(5).getReturnValue());
+        //testing adding a number into the end of the array
+        A1.add(6,"end");
+        assertEquals("start", A1.get(0).getReturnValue());
+        assertEquals(3, A1.get(1).getReturnValue());
+        assertEquals(4, A1.get(2).getReturnValue());
+        assertEquals("stinky", A1.get(3).getReturnValue());
+        assertEquals("poopie", A1.get(4).getReturnValue());
+        assertEquals("poos", A1.get(5).getReturnValue());
+        assertEquals("end", A1.get(6).getReturnValue());
+    }
+    @Test
+    public void removeTest() {
+        assertEquals(ErrorMessage.EMPTY_STRUCTURE, A1.remove(0).getError());
+        //assertEquals(ErrorMessage.INDEX_OUT_OF_BOUNDS, A1.remove(1).getError());
+        //assertEquals(ErrorMessage.INDEX_OUT_OF_BOUNDS, A1.remove(-1).getError());
+        A1.add("stinky");
+        A1.add("poopie");
+        A1.add("poos");
+        assertEquals("stinky", A1.get(0).getReturnValue());
+        assertEquals("poopie", A1.get(1).getReturnValue());
+        assertEquals("poos", A1.get(2).getReturnValue());
+        //Test remove from middle
+        A1.remove(1);
+        assertEquals("stinky", A1.get(0).getReturnValue());
+        assertEquals("poos", A1.get(1).getReturnValue());
+        assertEquals(2, A1.size());
+        assertEquals(ErrorMessage.INDEX_OUT_OF_BOUNDS, A1.get(2).getError());
+        assertEquals(ErrorMessage.INDEX_OUT_OF_BOUNDS, A1.get(-1).getError());
+        //Tests remove from end
+        A1.remove(1);
+        assertEquals("stinky", A1.get(0).getReturnValue());
+        assertEquals(1, A1.size());
+        //Tests removing last element
+        A1.remove(0);
+        assertEquals(ErrorMessage.EMPTY_STRUCTURE, A1.get(0).getError());
+        assertEquals(0, A1.size());
+    }
+    @Test
+    public void extendArrayTest() {
+        assertEquals(0, A1.size());
+        A1.extendArray();
+        assertEquals(1, A1.size());
+        A1.extendArray();
+        assertEquals(2, A1.size());
+    }
+}
+
